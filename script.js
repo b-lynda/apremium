e15f473195ed698eaea9f8395bc893d655d2f632
b-lynda <bcb.lynda@gmail.com>
2025-12-07 14:37:26 +0100

    add services section with cards

---
+++ script.js
@@ -0,0 +1,10 @@
+
+      const nav = document.querySelector('nav');
+      
+      window.addEventListener('scroll', () => {
+        if (window.scrollY > 100) {
+          nav.classList.add('bg-white', 'shadow-md');
+        } else {
+          nav.classList.remove('bg-white', 'shadow-md');
+        }
+      });
